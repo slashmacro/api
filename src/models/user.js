a11d1c403e2ce917c3fd23c87c193bd6260f4f82
Zach Harris <zachbharris94@gmail.com>
2020-01-04 23:00:40 -0600

    updates

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  })
+  return User
+}
+
+export default user
